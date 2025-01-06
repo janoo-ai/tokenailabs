6487a29cebfa4f9af2f03293e5514d948eb425cc
VKFarde <vedantfarde@gmail.com>
2025-01-06 12:03:44 +0530

    added html file I

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,12 +30,16 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="google-site-verification"
+          content="a_n4bITOnAvdBZrAHO34-fBuEsKejMvBx3nmVRfPzHw"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen flex flex-col items-center overflow-x-hidden bg-none`}>
         <Navbar className={"z-50"} />
         {children}
-        <Script
+        {/* <Script
           id="chatbot-config"
           strategy="beforeInteractive" // Or "afterInteractive" depending on when you want the script to run
         >
@@ -52,7 +56,7 @@ export default function RootLayout({ children }) {
           src="https://jaano.ai/chatbot-embedding.js"
           defer
           strategy="beforeInteractive"
-          ed></Script>
+          ed></Script> */}
       </body>
     </html>
   );
